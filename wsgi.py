c666b455349652519c8ea1782bd52349c5c64b14
Julien ALARCON <alarcon.julien@gmail.com>
2020-10-06 15:56:02 +0200

    Score added

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,18 @@
 # pylint: disable=missing-docstring
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from game import Game
 
 app = Flask(__name__)
+SESSION_TYPE='filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def home():
     game = Game()
+    score = session.get('score', 0)
     return render_template('home.html', grid=game.grid)
 
 @app.route('/check', methods=["POST"])
@@ -16,4 +21,8 @@ def check():
     game.grid = list(request.form['grid'])
     word = request.form["word"]
     is_valid = game.is_valid(word)
+
+    if is_valid:
+        session['score'] = session.get('score', 0) + 1
+
     return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
